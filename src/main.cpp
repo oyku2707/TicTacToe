6634c4927400d515c305ea1d95b8bb4f9e067b34
Öykü Ertuna <ertunaoyku@gmail.com>
2026-05-10 19:46:26 +0300

    İlk dikey ızgara çizgisi eklendi.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,13 @@
 int main() {
     // 600x600 boyutunda bir pencere oluşturuyoruz
     sf::RenderWindow pencere (sf::VideoMode({600, 600}), "Tic Tac Toe");
+    
+    //Ilk dikey cizgi nesnesi olustur.mak
+    sf::RectangleShape dikeyCizgi1;
+    dikeyCizgi1.setSize(sf::Vector2f(10.f,600.f));
+    dikeyCizgi1.setFillColor(sf::Color::Black);
+    dikeyCizgi1.setPosition(sf::Vector2(200.f,0.f));
+
 
     while (pencere.isOpen()) {
         while (const std::optional event =pencere.pollEvent()) {
@@ -13,6 +20,8 @@ int main() {
         }
         //pencereyi temizle(beyaz yap)
         pencere.clear(sf::Color::White);
+
+        pencere.draw(dikeyCizgi1);
 
         // Çizilenleri ekrana yansıt
         pencere.display();
