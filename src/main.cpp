948b20cff013b55cc2560636663cae25341de007
Öykü Ertuna <ertunaoyku@gmail.com>
2026-05-10 20:52:14 +0300

    Izgara yapısı tamamlandı ve arka plan açık gri yapıldı.

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,23 @@ int main() {
     // 600x600 boyutunda bir pencere oluşturuyoruz
     sf::RenderWindow pencere (sf::VideoMode({600, 600}), "Tic Tac Toe");
     
-    //Ilk dikey cizgi nesnesi olustur.mak
-    sf::RectangleShape dikeyCizgi1;
-    dikeyCizgi1.setSize(sf::Vector2f(10.f,600.f));
-    dikeyCizgi1.setFillColor(sf::Color::Black);
-    dikeyCizgi1.setPosition(sf::Vector2(200.f,0.f));
+    std::vector<sf::RectangleShape> cizgiler;
 
+    for(int i=0;i<2;i++){
+        // Dikey Çizgiler
+        sf::RectangleShape dikey;
+        dikey.setSize(sf::Vector2f(10.f, 600.f));
+        dikey.setFillColor(sf::Color::Black);
+        dikey.setPosition(sf::Vector2f(200.f * (i + 1), 0.f));
+        cizgiler.push_back(dikey);
+
+        // Yatay Çizgiler
+        sf::RectangleShape yatay;
+        yatay.setSize(sf::Vector2f(600.f, 10.f));
+        yatay.setFillColor(sf::Color::Black);
+        yatay.setPosition(sf::Vector2f(0.f, 200.f * (i + 1)));
+        cizgiler.push_back(yatay);
+    }
 
     while (pencere.isOpen()) {
         while (const std::optional event =pencere.pollEvent()) {
@@ -19,10 +30,11 @@ int main() {
             }
         }
         //pencereyi temizle(beyaz yap)
-        pencere.clear(sf::Color::White);
+        pencere.clear(sf::Color(211,211,211));
 
-        pencere.draw(dikeyCizgi1);
-
+        for(const auto& cizgi:cizgiler){
+            pencere.draw(cizgi);
+        }
         // Çizilenleri ekrana yansıt
         pencere.display();
     }
