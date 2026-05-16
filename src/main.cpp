7678fc12815bc30da49541b612f414bec1a6fdf9
Öykü Ertuna <ertunaoyku@gmail.com>
2026-05-16 20:13:36 +0300

    mantık:kazanma ve beraberlik kontrolü fonksiyonu sisteme eklendi.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,34 @@
 #include <optional>
 #include <vector>
 
+int kazananiKontrolEt(const int tahta[3][3]) {
+    for (int i = 0; i < 3; i++) {//Satir kontrolü
+        if (tahta[i][0] != 0 && tahta[i][0] == tahta[i][1] && tahta[i][1] == tahta[i][2]) {
+            return tahta[i][0];
+        }
+    }
+    for (int j = 0; j < 3; j++) {//sütun kontrolü
+        if (tahta[0][j] != 0 && tahta[0][j] == tahta[1][j] && tahta[1][j] == tahta[2][j]) {
+            return tahta[0][j];
+        }
+    }
+    if (tahta[0][0] != 0 && tahta[0][0] == tahta[1][1] && tahta[1][1] == tahta[2][2]) {//çapraz kontrolü 
+        return tahta[0][0];
+    }
+    if (tahta[0][2] != 0 && tahta[0][2] == tahta[1][1] && tahta[1][1] == tahta[2][0]) {//çapraz kontrolü
+        return tahta[0][2];
+    }
+    bool bosHucreVarMi = false;//Beraberlik için bos hücre kontrolü
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (tahta[i][j] == 0) bosHucreVarMi = true;
+        }
+    }
+    if (!bosHucreVarMi) return 3;//3=berabelik
+
+    return 0;//0=oyun devam ediyor
+}
+
 int main() {
     // 600x600 boyutunda bir pencere oluşturuyoruz
     sf::RenderWindow pencere(sf::VideoMode({600, 600}), "Tic Tac Toe");
@@ -95,19 +123,21 @@ int main() {
                          daireler.push_back(daire);
 
                          siraKimde=1;//sıra x'e geçer
-                      }      
-                  }
-                    //kontrol için terminale yazdırma
-                    std::cout << "\n--- TIKLAMA SONRASI TAHTA ---" << std::endl;
-                    for (int i = 0; i < 3; i++) {
-                        for (int j = 0; j < 3; j++) {
-                            std::cout << tahta[i][j] << " ";
-                        }
-                        std::cout << std::endl;
+                         }      
+                  
+                         int durum = kazananiKontrolEt(tahta);
+
+                         if (durum == 1) {
+                         std::cout << "TEBRİKLER! X OYUNCUSU KAZANDI!" << std::endl;
+                         pencere.close(); 
+                         } else if (durum == 2) {
+                         std::cout << "TEBRİKLER! O OYUNCUSU KAZANDI!" << std::endl;
+                         pencere.close();
+                         } else if (durum == 3) {
+                         std::cout << "OYUN BERABERE BİTTİ!" << std::endl;
+                          pencere.close();
+                         }
                     }
-                    std::cout << "-----------------------------" << std::endl;
-                    std::cout << "Siradaki Oyuncu: " << (siraKimde == 1 ? "X" : "O") << std::endl;
-                    
                 }
             }
         }
